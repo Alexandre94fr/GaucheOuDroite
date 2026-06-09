d32b8003a1b1529bd3b3578bf55b13028f855884
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-06-09 13:33:56 +0200

    🎫 ▸ Renaming correctly the DataBaseContext properties (check desc)
    The names of the BackEnd DataBaseContext's properties where not the same as the DataBase's Tables.
    
    Things done:
    - User -> Users
    - UserProgression -> UserProgressions
    - Level -> Levels
    - LevelResponseTimeStep -> LevelResponseTimeSteps

--- ProjectFiles/BackEnd/GaucheOuDroiteBackEnd/Data/DataBaseContext.cs
+++ ProjectFiles/BackEnd/GaucheOuDroiteBackEnd/Data/DataBaseContext.cs
@@ -4,12 +4,12 @@ namespace GaucheOuDroiteBackEnd.Data
 {
     public class DataBaseContext(DbContextOptions<DataBaseContext> p_options) : DbContext(p_options)
     {
-        public DbSet<Models.User> User { get; set; } = default!;
+        public DbSet<Models.User> Users { get; set; } = default!;
 
-        public DbSet<Models.UserProgression> UserProgression { get; set; } = default!;
+        public DbSet<Models.UserProgression> UserProgressions { get; set; } = default!;
 
-        public DbSet<Models.Level> Level { get; set; } = default!;
+        public DbSet<Models.Level> Levels { get; set; } = default!;
 
-        public DbSet<Models.LevelResponseTimeStep> LevelResponseTimeStep { get; set; } = default!;
+        public DbSet<Models.LevelResponseTimeStep> LevelResponseTimeSteps { get; set; } = default!;
     }
 }
