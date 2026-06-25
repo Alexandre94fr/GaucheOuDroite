cd2567cb2682c2e126ead0fd632780a42f72e977
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-06-25 11:32:09 +0200

    📜 ▸ Creating the LevelDifficultyProperties static Class (check desc)
    The LevelDifficultyProperties static Class is only storing LevelDifficulty constants.
    
    These includes:
    
    - The color associated with a difficulty
    - The French translation of the difficulties

---
+++ ProjectFiles/FrontEnd/GaucheOuDroite/Assets/Shared/Constants/LevelDifficultyProperties.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Numerics;
+
+using Shared.Enums;
+
+
+namespace Shared.Constants
+{
+    public static class LevelDifficultyProperties
+    {
+        public static readonly Dictionary<LevelDifficulty, Vector3> DIFFICULTY_COLORS = new()
+        {
+            [LevelDifficulty.Easy]              = new(0.0f , 0.5f , 0.05f),
+            [LevelDifficulty.Medium]            = new(0.0f , 0.45f, 0.50f),
+            [LevelDifficulty.Hard]              = new(0.5f , 0.0f , 0.0f ),
+
+            [LevelDifficulty.Progressive]       = new(0.45f, 0.0f , 0.05f),
+        };
+
+        // -- Localization -- //
+
+        public static readonly Dictionary<LevelDifficulty, string> DIFFICULTY_NAMES_IN_FRENCH = new()
+        {
+            [LevelDifficulty.Easy]              = "Facile",
+            [LevelDifficulty.Medium]            = "Normal",
+            [LevelDifficulty.Hard]              = "Difficile",
+
+            [LevelDifficulty.Progressive]       = "Progressif",
+        };
+    }
+}
