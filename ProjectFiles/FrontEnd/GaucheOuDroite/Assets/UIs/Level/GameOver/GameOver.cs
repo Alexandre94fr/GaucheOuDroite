6f494d31a9b04ab49ceb5dd31787a2bcf7f4a697
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-07-03 10:34:57 +0200

    📜 ▸ Implementing DirectionText Script (check desc)
    Handles showing itself when the OnLevelWonEvent or OnLevelLostEvent Events are fired.
    It also changes his texts to show the Level's name and that the player won or lost.
    
    For now the GameOver doesn't implement showing the player's score he gain while playing the Level.

---
+++ ProjectFiles/FrontEnd/GaucheOuDroite/Assets/UIs/Level/GameOver/GameOver.cs
@@ -0,0 +1,57 @@
+using FrontEnd.Data.Game;
+using TMPro;
+using UnityEngine;
+
+using VariableCheckerPackage;
+
+
+public class GameOver : MonoBehaviour
+{
+    [Header("Internal references:")]
+    [SerializeField] GameObject _gameOverUIGameObject;
+
+    [Space]
+    [SerializeField] TextMeshProUGUI _levelNameText;
+    [SerializeField] TextMeshProUGUI _titleText;
+
+
+    void Start()
+    {
+        // -- Class properties verifications -- //
+
+        if (!VariablesChecker.AreVariablesValid(name, null,
+            (_gameOverUIGameObject, nameof(_gameOverUIGameObject)),
+
+            (_levelNameText, nameof(_levelNameText)),
+            (_titleText, nameof(_titleText))
+        )) return;
+
+        // -- Handling events -- //
+
+        EventHandler.OnLevelWonEvent += OnLevelWon;
+        EventHandler.OnLevelLostEvent += OnLevelLost;
+    }
+
+    void OnDestroy()
+    {
+        EventHandler.OnLevelWonEvent -= OnLevelWon;
+        EventHandler.OnLevelLostEvent -= OnLevelLost;
+    }
+
+
+    void OnLevelWon(Level p_levelProperties, int p_score)
+    {
+        _gameOverUIGameObject.SetActive(true);
+
+        _levelNameText.text = p_levelProperties.Name;
+        _titleText.text = "Terminé !";
+    }
+
+    void OnLevelLost(Level p_levelProperties, int p_score)
+    {
+        _gameOverUIGameObject.SetActive(true);
+
+        _levelNameText.text = p_levelProperties.Name;
+        _titleText.text = "Échoué !";
+    }
+}
