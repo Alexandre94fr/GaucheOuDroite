17bce1ade93c661980bf76f1e5fef58a468f8e06
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-07-08 16:54:09 +0200

    📜 ▸ Modifying DirectionButton Script (check desc)
    The DirectionButton Script now make his button un-interactable when the game is paused.

--- ProjectFiles/FrontEnd/GaucheOuDroite/Assets/UIs/Level/DirectionButton/DirectionButton.cs
+++ ProjectFiles/FrontEnd/GaucheOuDroite/Assets/UIs/Level/DirectionButton/DirectionButton.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
+
+using VariableCheckerPackage;
 
 
 public class DirectionButton : MonoBehaviour
 {
+    [Header("Internal references:")]
+    [SerializeField] Button _button;
+
     [Header("Properties:")]
     [SerializeField] DirectionProperties.Direction _buttonDirection;
 
-    
+
+    void Start()
+    {
+        // -- Class properties verifications -- //
+
+        if (!VariablesChecker.AreVariablesValid(name, null,
+            (_button, nameof(_button))
+        )) return;
+
+        // -- Handling events -- //
+
+        PauseManager.OnPauseEvent += OnPause;
+    }
+
+    void OnDestroy()
+    {
+        PauseManager.OnPauseEvent -= OnPause;
+    }
+
+
     public void OnButtonPressed()
     {
         EventHandler.OnDirectionChoiceInputEvent?.Invoke(_buttonDirection);
+    }
+
+
+    void OnPause(bool p_isGamePaused)
+    {
+        _button.interactable = !p_isGamePaused;
     }
 }
