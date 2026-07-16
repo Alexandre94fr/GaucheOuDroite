291d05bde848e7b243715bca46e66627876a2902
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-07-16 13:43:31 +0200

    📊 ▸ Fully translating the AuthenticationProperties in English

--- ProjectFiles/Shared/Constants/AuthenticationProperties.cs
+++ ProjectFiles/Shared/Constants/AuthenticationProperties.cs
@@ -59,10 +59,11 @@ namespace Shared.Constants
 
         public static readonly System.TimeSpan MAXIMAL_INITIAL_DATA_LOADING_TIME = new(0, 0, 10);
 
-        /// <summary>
-        /// The values of this dictionary are in french because the project is only in french.
-        /// </summary>
-        public static readonly Dictionary<AuthenticationErrorReasons, string> AUTHENTICATION_ERROR_MESSAGES = new()
+        // ------------ //
+        // -- FRENCH -- //
+        // ------------ //
+
+        public static readonly Dictionary<AuthenticationErrorReasons, string> AUTHENTICATION_ERROR_MESSAGES_IN_FRENCH = new()
         {
             [AuthenticationErrorReasons.UsernameIsEmpty]                            = $"Le pseudonyme est vide.",
             [AuthenticationErrorReasons.UsernameIsTooShort]                         = $"Le pseudonyme est trop court (<{USERNAME_MINIMUM_LENGHT}).",
@@ -95,7 +96,7 @@ namespace Shared.Constants
             [AuthenticationErrorReasons.InternalServerError]                        = $"Un problème interne a été détecté côté serveur.\nSi le problème persiste, relancez l'application.",
         };
 
-        public static readonly Dictionary<AuthenticationErrorReasons, string> USERNAME_REQUIREMENTS_HELPING_MESSAGES = new()
+        public static readonly Dictionary<AuthenticationErrorReasons, string> USERNAME_REQUIREMENTS_HELPING_MESSAGES_IN_FRENCH = new()
         {
             [AuthenticationErrorReasons.UsernameIsTooShort]                         = $"Doit faire au moins {USERNAME_MINIMUM_LENGHT} caractère(s).",
             [AuthenticationErrorReasons.UsernameIsTooLong]                          = $"Ne doit pas dépasser {USERNAME_MAXIMUM_LENGHT} caractère(s).",
@@ -103,7 +104,7 @@ namespace Shared.Constants
             [AuthenticationErrorReasons.UsernameContainAtLeastOneSpaceCharacter]    = $"Ne doit pas contenir d'espace.",
         };
 
-        public static readonly Dictionary<AuthenticationErrorReasons, string> PASSWORD_REQUIREMENTS_HELPING_MESSAGES = new()
+        public static readonly Dictionary<AuthenticationErrorReasons, string> PASSWORD_REQUIREMENTS_HELPING_MESSAGES_IN_FRENCH = new()
         {
             [AuthenticationErrorReasons.PasswordIsTooShort]                         = $"Doit faire au moins {PASSWORD_MINIMUM_LENGHT} caractère(s).",
             [AuthenticationErrorReasons.PasswordIsTooLong]                          = $"Ne doit pas dépasser {PASSWORD_MAXIMUM_LENGHT} caractère(s).",
@@ -116,25 +117,104 @@ namespace Shared.Constants
         };
 
 
-        public const string SERVER_CONNECTION_ERROR_MESSAGE                         = "Échec de la connection avec le serveur.\nLe serveur n'est peut-être pas lancé.\nSi le problème persiste, relancez l'application.";
+        public const string SERVER_CONNECTION_ERROR_MESSAGE_IN_FRENCH               = "Échec de la connection avec le serveur.\nLe serveur n'est peut-être pas lancé.\nSi le problème persiste, relancez l'application.";
         
-        public const string DATA_PROCESSING_ERROR_MESSAGE                           = "Erreur lors du traitement des données reçues.\nDonnées corrompues ou au mauvais format.\nSi le problème persiste, relancez l'application.";
+        public const string DATA_PROCESSING_ERROR_MESSAGE_IN_FRENCH                 = "Erreur lors du traitement des données reçues.\nDonnées corrompues ou au mauvais format.\nSi le problème persiste, relancez l'application.";
         
-        public const string UNKNOWN_ERROR_MESSAGE                                   = "Erreur inconnue.\nSi le problème persiste, relancez l'application.";
+        public const string UNKNOWN_ERROR_MESSAGE_IN_FRENCH                         = "Erreur inconnue.\nSi le problème persiste, relancez l'application.";
 
 
-        public static readonly string TOO_LONG_LOADING_DATA_ERROR_MESSAGE           = $"Temps maximal de chargement des données dépassé ({MAXIMAL_INITIAL_DATA_LOADING_TIME}s).\nSi le problème persiste, relancez l'application.";
+        public static readonly string TOO_LONG_LOADING_DATA_ERROR_MESSAGE_IN_FRENCH = $"Temps maximal de chargement des données dépassé ({MAXIMAL_INITIAL_DATA_LOADING_TIME}s).\nSi le problème persiste, relancez l'application.";
 
 
 
-        public const string SUCCESSFUL_LOCAL_AUTHENTICATION_MESSAGE                 = "Authentification validée localement.\nEnvoie des informations au serveur.";
-        public const string SUCCESSFUL_SERVER_AUTHENTICATION_MESSAGE                = "Authentification validée par le serveur.";
+        public const string SUCCESSFUL_LOCAL_AUTHENTICATION_MESSAGE_IN_FRENCH       = "Authentification validée localement.\nEnvoie des informations au serveur.";
+        public const string SUCCESSFUL_SERVER_AUTHENTICATION_MESSAGE_IN_FRENCH      = "Authentification validée par le serveur.";
 
 
         public static readonly Dictionary<AuthenticationMode, string> AUTHENTICATION_MODE_IN_FRENCH = new()
         {
             [AuthenticationMode.SignUp] = $"Inscription",
             [AuthenticationMode.LogIn] = $"Connexion",
+        };
+
+        // ------------- //
+        // -- ENGLISH -- //
+        // ------------- //
+
+        public static readonly Dictionary<AuthenticationErrorReasons, string> AUTHENTICATION_ERROR_MESSAGES_IN_ENGLISH = new()
+        {
+            [AuthenticationErrorReasons.UsernameIsEmpty] = $"The username is empty.",
+            [AuthenticationErrorReasons.UsernameIsTooShort] = $"The username is too short (<{USERNAME_MINIMUM_LENGHT}).",
+            [AuthenticationErrorReasons.UsernameIsTooLong] = $"The username is too long (>{USERNAME_MAXIMUM_LENGHT}).",
+
+            [AuthenticationErrorReasons.UsernameAlreadyExists] = $"The username is already taken by another user.\nAre you trying to log-in?",
+
+            [AuthenticationErrorReasons.UsernameContainAtLeastOneSpaceCharacter] = $"The username contain at least one space character.",
+
+            // --- //
+
+            [AuthenticationErrorReasons.PasswordIsEmpty] = $"The password is empty.",
+            [AuthenticationErrorReasons.PasswordIsTooShort] = $"The password is too short (<{PASSWORD_MAXIMUM_LENGHT}).",
+            [AuthenticationErrorReasons.PasswordIsTooLong] = $"The password is too long (>{PASSWORD_MAXIMUM_LENGHT}).",
+
+            [AuthenticationErrorReasons.PasswordContainAtLeastOneSpaceCharacter] = $"The password contain at least one space character.",
+
+            [AuthenticationErrorReasons.PasswordDoesNotContainAnyLetters] = $"The password doesn't contain any letters.",
+            [AuthenticationErrorReasons.PasswordDoesNotContainAnyNumbers] = $"The password doesn't contain any numbers.",
+            [AuthenticationErrorReasons.PasswordDoesNotContainAnySpecialCharacters] = $"The password doesn't contain any special characters.",
+
+            // --- //
+
+            [AuthenticationErrorReasons.UsernameDoesNotExist] = $"The username doesn't exist.\nAre you trying to create an account?",
+            [AuthenticationErrorReasons.IncorrectPassword] = $"Incorrect password\nPlease try again.",
+
+            [AuthenticationErrorReasons.UserAlreadyHasProgressions] = $"Some progression data have been detected on a user that shouldn't have any.\nPlease try again.",
+            [AuthenticationErrorReasons.NoUserProgressionsFound] = $"No progression data have been founded on a user that should have some.\nPlease try again.",
+
+            [AuthenticationErrorReasons.InternalServerError] = $"An internal problem has been detected on the serverr.\nIf the problem persists, restart the application.",
+        };
+
+        public static readonly Dictionary<AuthenticationErrorReasons, string> USERNAME_REQUIREMENTS_HELPING_MESSAGES_IN_ENGLISH = new()
+        {
+            [AuthenticationErrorReasons.UsernameIsTooShort] = $"Must be at least {USERNAME_MINIMUM_LENGHT} character(s) long.",
+            [AuthenticationErrorReasons.UsernameIsTooLong] = $"Must not be more than {USERNAME_MAXIMUM_LENGHT} character(s) long.",
+
+            [AuthenticationErrorReasons.UsernameContainAtLeastOneSpaceCharacter] = $"Must not contain space characters.",
+        };
+
+        public static readonly Dictionary<AuthenticationErrorReasons, string> PASSWORD_REQUIREMENTS_HELPING_MESSAGES_IN_ENGLISH = new()
+        {
+            [AuthenticationErrorReasons.PasswordIsTooShort] = $"Must be at least {USERNAME_MINIMUM_LENGHT} character(s) long.",
+            [AuthenticationErrorReasons.PasswordIsTooLong] = $"Must not be more than {USERNAME_MAXIMUM_LENGHT} character(s) long.",
+
+            [AuthenticationErrorReasons.PasswordContainAtLeastOneSpaceCharacter] = $"Must not contain space characters.",
+
+            [AuthenticationErrorReasons.PasswordDoesNotContainAnyLetters] = $"Must contain a least one letter.",
+            [AuthenticationErrorReasons.PasswordDoesNotContainAnyNumbers] = $"Must contain a least one number.",
+            [AuthenticationErrorReasons.PasswordDoesNotContainAnySpecialCharacters] = $"Must contain a least one special character.",
+        };
+
+
+        public const string SERVER_CONNECTION_ERROR_MESSAGE_IN_ENGLISH = "Failed to connect to the server.\nThe server may not be running.\nIf the problem persists, restart the application.";
+
+        public const string DATA_PROCESSING_ERROR_MESSAGE_IN_ENGLISH = "Error processing the received data.\nThe data is corrupted or in the wrong format.\nIf the problem persists, restart the application.";
+
+        public const string UNKNOWN_ERROR_MESSAGE_IN_ENGLISH = "Unknown error.\nIf the problem persists, restart the application.";
+
+
+        public static readonly string TOO_LONG_LOADING_DATA_ERROR_MESSAGE_IN_ENGLISH = $"The maximum data loading time has been exceeded ({MAXIMAL_INITIAL_DATA_LOADING_TIME}s).\nIf the problem persists, restart the application.";
+
+
+
+        public const string SUCCESSFUL_LOCAL_AUTHENTICATION_MESSAGE_IN_ENGLISH = "Authentication validated locally.\nSending information to the server.";
+        public const string SUCCESSFUL_SERVER_AUTHENTICATION_MESSAGE_IN_ENGLISH = "Authentication validated by the server.";
+
+
+        public static readonly Dictionary<AuthenticationMode, string> AUTHENTICATION_MODE_IN_ENGLISH = new()
+        {
+            [AuthenticationMode.SignUp] = $"Sign up",
+            [AuthenticationMode.LogIn] = $"Log in",
         };
     }
 }
