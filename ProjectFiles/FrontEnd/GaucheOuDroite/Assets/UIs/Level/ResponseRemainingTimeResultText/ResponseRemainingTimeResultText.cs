a7f50ba2e929c7599a48a3b3de036057942b01e5
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-07-20 12:23:46 +0200

    📱 ▸ Making ResponseRemainingText change rotation each response

--- ProjectFiles/FrontEnd/GaucheOuDroite/Assets/UIs/Level/ResponseRemainingTimeResultText/ResponseRemainingTimeResultText.cs
+++ ProjectFiles/FrontEnd/GaucheOuDroite/Assets/UIs/Level/ResponseRemainingTimeResultText/ResponseRemainingTimeResultText.cs
@@ -1,5 +1,5 @@
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 using VariableCheckerPackage;
 
@@ -9,6 +9,10 @@ public class ResponseRemainingTimeResultText : MonoBehaviour
     [Header("Internal references:")]
     [SerializeField] TextMeshProUGUI _responseRemainingTimeResultText;
     [SerializeField] TextMeshProUGUI _addedScoreText;
+
+    [Header("Properties:")]
+    [SerializeField] float _minimumRandomRotation = -5;
+    [SerializeField] float _maximumRandomRotation = 5;
 
 
     void Start()
@@ -41,7 +45,30 @@ public class ResponseRemainingTimeResultText : MonoBehaviour
 
         _responseRemainingTimeResultText.text = ResponseProperties.RESPONSE_REMAINING_TIME_RESULT_NAMES_IN_FRENCH[p_responseResult.Result];
         _responseRemainingTimeResultText.color = ResponseProperties.RESPONSE_REMAINING_TIME_RESULT_COLORS[p_responseResult.Result];
+        _responseRemainingTimeResultText.transform.parent.GetComponent<RectTransform>().localScale = ResponseProperties.RESPONSE_REMAINING_TIME_RESULT_SCALE_SIZES[p_responseResult.Result];
+
+        RandomizeRotation();
 
         _addedScoreText.text = "+" + p_responseResult.AddedScore;
+    }
+
+    void RandomizeRotation()
+    {
+        RectTransform responseRemainingTimeResultTextRectTransform = _responseRemainingTimeResultText.GetComponent<RectTransform>();
+        RectTransform addedScoreTextRectTransform = _addedScoreText.GetComponent<RectTransform>();
+
+        float randomRotation = UnityEngine.Random.Range(_minimumRandomRotation, _maximumRandomRotation);
+
+        responseRemainingTimeResultTextRectTransform.rotation = Quaternion.Euler(
+            responseRemainingTimeResultTextRectTransform.rotation.x,
+            responseRemainingTimeResultTextRectTransform.rotation.y,
+            randomRotation
+        );
+
+        addedScoreTextRectTransform.rotation = Quaternion.Euler(
+            addedScoreTextRectTransform.rotation.x,
+            addedScoreTextRectTransform.rotation.y,
+            randomRotation
+        );
     }
 }
