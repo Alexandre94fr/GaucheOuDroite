f24e320d5a796d4c040f1505f222c31579043c39
Alexandre94fr <Alexandre94.fr@gmail.com>
2026-09-18 11:41:24 +0200

    📊 ▸ Adding VeryEasy properties in LevelDifficultyProperties

--- ProjectFiles/Shared/Constants/LevelDifficultyProperties.cs
+++ ProjectFiles/Shared/Constants/LevelDifficultyProperties.cs
@@ -10,6 +10,7 @@ namespace Shared.Constants
     {
         public static readonly Dictionary<LevelDifficulty, Vector3> DIFFICULTY_COLORS = new()
         {
+            [LevelDifficulty.VeryEasy]      = new(0.00f, 0.65f, 0.00f),
             [LevelDifficulty.Easy]          = new(0.00f, 0.50f, 0.05f),
             [LevelDifficulty.Medium]        = new(0.45f, 0.50f, 0.00f),
             [LevelDifficulty.Hard]          = new(0.50f, 0.00f, 0.00f),
@@ -21,6 +22,7 @@ namespace Shared.Constants
 
         public static readonly Dictionary<LevelDifficulty, string> DIFFICULTY_NAMES_IN_FRENCH = new()
         {
+            [LevelDifficulty.VeryEasy]      = "Très facile",
             [LevelDifficulty.Easy]          = "Facile",
             [LevelDifficulty.Medium]        = "Normal",
             [LevelDifficulty.Hard]          = "Difficile",
@@ -30,6 +32,7 @@ namespace Shared.Constants
 
         public static readonly Dictionary<LevelDifficulty, string> DIFFICULTY_NAMES_IN_ENGLISH = new()
         {
+            [LevelDifficulty.VeryEasy]      = "Very easy",
             [LevelDifficulty.Easy]          = "Easy",
             [LevelDifficulty.Medium]        = "Medium",
             [LevelDifficulty.Hard]          = "Hard",
